c711b5505a27af5a8a8fb18a5c9ead4cceac6836
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-05-30 12:51:13 +0530

    [Priyanshu] added address Array

--- Address.js
+++ Address.js
@@ -98,3 +98,17 @@ try
 catch(e){
     console.error(e);
 }
+//UC3-Defining an address book array and storing the addresses
+let addressArr = new Array();
+try
+{
+    let address1 = new AddressBook("Ravi","Singh","UP65","VNS","UP",838372,9393932090,"ravi@gmail.com");
+    let address2= new AddressBook("Amit","Patel","GJ66","Surat","Gujarat",612302,9982705303,"amit@gmail.com")
+    addressArr.push(address1);
+    addressArr.push(address2);
+    console.log(addressArr);
+}
+catch(e)
+{
+    console.error(e);
+}
