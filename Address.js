e6945196302097330845dee9b4a99bfbda482082
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-05-30 13:08:40 +0530

    [Priyanshu] added get count of address book

--- Address.js
+++ Address.js
@@ -130,3 +130,13 @@ function DeleteAddress(addressbook){
 }
 let deleteIndex=addressArr.find(DeleteAddress);
 console.log(addressArr.splice(deleteIndex,1)+" is deleted")
+
+//UC6-Getting count of total address 
+let n=0;
+function GetTotal(a){
+   if(a!=null){
+       ++n;
+   }
+   return n;
+}
+console.log("Total count of addresses "+addressArr.reduce(GetTotal,0));
