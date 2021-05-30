ddea06f3c1b6ac35e8efd72a77d1749599265157
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-05-30 13:04:33 +0530

    [Priyanshu] added delete using name

--- Address.js
+++ Address.js
@@ -118,7 +118,15 @@ catch(e)
 //UC4-Searching an address by using the firstname
 function SearchName(addressbook)
 {
-    if(addressbook.firstName=="Shiva") return addressbook;
+    if(addressbook.firstName=="Amit") return addressbook;
 }
 let searchResult =addressArr.find(SearchName);
 console.log("Result after searching "+searchResult);
+//UC5-Delete an element using the firstname
+function DeleteAddress(addressbook){
+    let resultAddress = SearchName(addressbook);
+    var index= addressArr.indexOf(resultAddress);
+    return index;
+}
+let deleteIndex=addressArr.find(DeleteAddress);
+console.log(addressArr.splice(deleteIndex,1)+" is deleted")
